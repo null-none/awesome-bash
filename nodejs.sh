29e98e003f46a15eda25373e4d53bdea2a61cf40
Dmitry Kalinin <kal1sha.ua@gmail.com>
2016-11-09 14:58:16 +0200

    Create nodejs.sh

---
+++ nodejs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Install nodejs
+
+sudo apt-get -y update
+sudo apt-get -y install nodejs
+sudo apt-get -y install npm
+sudo apt-get -y install build-essential libssl-dev
+sudo apt-get -y install nodejs-legacy
